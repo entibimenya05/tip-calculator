bbed5c69fe129085d3d5dd794da8a6fb998a4f09
emmanuel <entibimenya03@gmail.com>
2025-02-04 15:12:13 +0000

    wrap up state management with lift up state, derived state by calculating tip, with ti-calculator exercise

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function App() {
   return (
     <div className="App">
@@ -6,29 +8,62 @@ export default function App() {
   );
 }
 function TipCalculator() {
+  //1.lift up state instead of setting up state locally
+  const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
+  //to make the reset butyton work
+  function handleReset() {
+    setBill("");
+    setPercentage1(0);
+    setPercentage2(0);
+  }
   return (
     <div>
-      <BillInput />
-      <SelectPercentage>How did you like the service</SelectPercentage>
-      <SelectPercentage>How did your friend like the service?</SelectPercentage>
-      <Output />
-      <Reset />
+      {/*2.pass in a props called bill with a value of bill*/}
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPercentage percentage={percentage1} onSelect={setPercentage1}>
+        How did you like the service
+      </SelectPercentage>
+      <SelectPercentage percentage={percentage2} onSelect={setPercentage2}>
+        How did your friend like the service?
+      </SelectPercentage>
+      {/*in order not to display the button when there no bill also use fragment because there are two elements to display*/}
+      {bill > 0 && (
+        <>
+          <Output bill={bill} tip={tip} />
+          <Reset onReset={handleReset} />
+        </>
+      )}
     </div>
   );
 }
-function BillInput() {
+//3.below receiove these props:bill,onSetBill
+function BillInput({ bill, onSetBill }) {
   return (
     <div>
       <label>How much was the bill?</label>
-      <input type="text" placeholder="Bill balue" />
+      {/*4. let now use these value*/}
+      <input
+        type="text"
+        placeholder="Bill balue"
+        value={bill}
+        //Number(e.target.value) convert the whole thing to a number
+        onChange={(e) => onSetBill(Number(e.target.value))}
+      />
     </div>
   );
 }
-function SelectPercentage({ children }) {
+function SelectPercentage({ children, percentage, onSelect }) {
   return (
     <div>
       <label>{children}</label>
-      <select>
+      {/*finally pass in the value to the select*/}
+      <select
+        value={percentage}
+        onChange={(e) => onSelect(Number(e.target.value))}
+      >
         <option valu="0">Dissatisfied (%)</option>
         <option value="5">It was Okay (5%)</option>
         <option value="10">It was Good (10%)</option>
@@ -37,13 +72,15 @@ function SelectPercentage({ children }) {
     </div>
   );
 }
-function Output() {
+function Output({ bill, tip }) {
   return (
     <div>
-      <h3>YOU Ppay x($A+$B tip)</h3>
+      <h3>
+        YOU pay ${bill + tip} (${bill}+${tip} tip)
+      </h3>
     </div>
   );
 }
-function Reset() {
-  return <button>Reset</button>;
+function Reset({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
 }
